cfb6d17a2aa54ba935d26cfc5e886a1e8c622696
Андрей Посессор <posessoras@gmail.com>
2026-09-10 23:14:52 +0300

    Добавлена вложенная валидация объектов Map

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -6,6 +6,7 @@ import java.util.Map;
  * Схема для валидации объектов Map.
  */
 public class MapSchema extends BaseSchema<Map<?, ?>> {
+
   public MapSchema required() {
     addCheck("required", value -> value != null);
     return this;
@@ -13,6 +14,31 @@ public class MapSchema extends BaseSchema<Map<?, ?>> {
 
   public MapSchema sizeof(int size) {
     addCheck("sizeof", value -> value == null || value.size() == size);
+    return this;
+  }
+
+  /**
+   * Добавляет схему валидации для значений объекта Map.
+   *
+   * @param schemas набор схем для проверки значений по ключам
+   * @return текущая схема
+   */
+  public MapSchema shape(Map<String, BaseSchema<String>> schemas) {
+    addCheck("shape", value -> {
+      if (value == null) {
+        return true;
+      }
+      for (Map.Entry<String, BaseSchema<String>> entry : schemas.entrySet()) {
+        String key = entry.getKey();
+        BaseSchema<String> fieldSchema = entry.getValue();
+
+        String fieldValue = (String) value.get(key);
+        if (!fieldSchema.isValid(fieldValue)) {
+          return false;
+        }
+      }
+      return true;
+    });
     return this;
   }
 }
--- app/src/test/java/hexlet/code/MapSchemaTest.java
+++ app/src/test/java/hexlet/code/MapSchemaTest.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import hexlet.code.schemas.BaseSchema;
 import hexlet.code.schemas.MapSchema;
 import java.util.HashMap;
 import java.util.Map;
@@ -38,5 +39,40 @@ public class MapSchemaTest {
     assertTrue(schema.isValid(data));
     schema.sizeof(1);
     assertFalse(schema.isValid(data));
+  }
+
+  @Test
+  void testShape() {
+    Validator v = new Validator();
+    MapSchema schema = v.map();
+
+    Map<String, BaseSchema<String>> schemas = new HashMap<>();
+    schemas.put("firstName", v.string().required().contains("oh"));
+    schemas.put("lastName", v.string().required().minLength(2));
+    schema.shape(schemas);
+
+    Map<String, String> human1 = new HashMap<>();
+    human1.put("firstName", "John");
+    human1.put("lastName", "Smith");
+    assertTrue(schema.isValid(human1));
+
+    Map<String, String> human2 = new HashMap<>();
+    human2.put("firstName", "John");
+    human2.put("lastName", null);
+    assertFalse(schema.isValid(human2));
+
+    Map<String, String> human3 = new HashMap<>();
+    human3.put("firstName", "John");
+    human3.put("lastName", "B");
+    assertFalse(schema.isValid(human3));
+
+    Map<String, String> human4 = new HashMap<>();
+    human4.put("firstName", "John");
+    assertFalse(schema.isValid(human4));
+
+    Map<String, String> human5 = new HashMap<>();
+    human5.put("firstName", "Anna");
+    human5.put("lastName", "Smith");
+    assertFalse(schema.isValid(human5));
   }
 }
